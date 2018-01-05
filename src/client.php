70db7d18cee197e299926426cc3173a3c68d705d
Joe <jrosenwald@getsidecar.com>
2018-01-05 14:14:17 -0500

    docblocks

--- src/client.php
+++ src/client.php
@@ -11,16 +11,32 @@ function newRequest($method, $uri, $options = [])
 
 class Client
 {
+    /**
+     * @var $endpoint_
+     */
     protected $endpoint_;
 
+    /**
+     * @var $client_
+     */
     protected $client_;
 
+    /**
+     * Client constructor.
+     * @param $client
+     * @param $endpoint
+     */
     public function __construct($client, $endpoint)
     {
         $this->client_ = $client;
         $this->endpoint_ = $endpoint;
     }
 
+    /**
+     * @param $partner
+     * @param $accountId
+     * @return mixed
+     */
     public function getCampaigns($partner, $accountId)
     {
         $uri = sprintf("%s/v1/%s/accounts/%s/campaigns", $this->endpoint_, $partner, $accountId);
@@ -29,6 +45,12 @@ class Client
         return $this->doRequest($req);
     }
 
+    /**
+     * @param $partner
+     * @param $accountId
+     * @param $campaignId
+     * @return mixed
+     */
     public function getCampaignAdGroups($partner, $accountId, $campaignId)
     {
         $uri = sprintf(
@@ -43,6 +65,12 @@ class Client
         return $this->doRequest($req);
     }
 
+    /**
+     * @param $partner
+     * @param $accountId
+     * @param $adGroupId
+     * @return mixed
+     */
     public function getAdGroupPartitions($partner, $accountId, $adGroupId)
     {
         $uri = sprintf(
@@ -57,6 +85,12 @@ class Client
         return $this->doRequest($req);
     }
 
+    /**
+     * @param $partner
+     * @param $accountId
+     * @param $campaign
+     * @return mixed
+     */
     public function getCampaignCriteria($partner, $accountId, $campaign)
     {
         $uri = sprintf("%s/v1/%s/accounts/%s/campaigns/%d/criteria", $this->endpoint_, $partner, $accountId, $campaign);
@@ -65,6 +99,11 @@ class Client
         return $this->doRequest($req);
     }
 
+    /**
+     * @param $req
+     * @return mixed
+     * @throws \Exception
+     */
     private function doRequest($req)
     {
         $res = $this->client_->send($req, ['http_errors' => false]);
